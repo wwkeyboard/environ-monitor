a74d066cb54887b8b7d18d84363cccc4ffb0e34b
Aaron Lee <alee@aaronosaur.us>
2020-05-25 15:31:47 -0500

    fit prom's requiremenst

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use bme280::BME280;
 use linux_embedded_hal::{Delay, I2cdev};
 use std::sync::Mutex;
 
-#[get("/measure")]
+#[get("/metrics")]
 async fn index() -> impl Responder {
     let i2c_bus = I2cdev::new("/dev/i2c-1").unwrap();
 
@@ -12,10 +12,7 @@ async fn index() -> impl Responder {
     bme280.init().unwrap();
 
     let m = bme280.measure().unwrap();
-    format!(
-        "humid: {}\ntemp: {}\npres: {}\n",
-        m.humidity, m.temperature, m.pressure
-    )
+    format!("env_humidity {}\nenv_temperature {}\nenv_pressure {}\n", m.humidity, m.temperature, m.pressure)
 }
 
 #[actix_rt::main]
