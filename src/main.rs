524324944d0fb4bf22beb69e6ecb7949ec54b886
Aaron Lee <alee@aaronosaur.us>
2020-05-29 17:32:50 -0500

    better formating

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,26 @@ use linux_embedded_hal::{Delay, I2cdev};
 use std::sync::Mutex;
 
 struct Sensor {
+    sensor: BME280,
+}
 
+impl Sensor {
+    fn new() {
+        let i2c_bus = I2cdev::new("/dev/i2c-1").unwrap();
+
+        let mut bme280 = BME280::new_secondary(i2c_bus, Delay);
+
+        bme280.init().unwrap();
+    }
 }
 
 #[get("/metrics")]
 async fn index() -> impl Responder {
-    let i2c_bus = I2cdev::new("/dev/i2c-1").unwrap();
-
-    let mut bme280 = BME280::new_secondary(i2c_bus, Delay);
-
-    bme280.init().unwrap();
-
     let m = bme280.measure().unwrap();
-    format!("env_humidity {}\nenv_temperature {}\nenv_pressure {}\n", m.humidity, m.temperature, m.pressure)
+    format!(
+        "env_humidity {}\nenv_temperature {}\nenv_pressure {}\n",
+        m.humidity, m.temperature, m.pressure
+    )
 }
 
 #[actix_rt::main]
